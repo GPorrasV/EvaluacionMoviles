1cf2faed76b654ad28556a0084cc4406f6a5e0a4
Geraldine <geraldine.porras13474@est.uca.edu.ni>
2023-03-29 20:20:48 -0600

    AndroidApp

---
+++ app/src/main/java/com/example/myuca/Coordinador.kt
@@ -0,0 +1,10 @@
+package com.example.myuca
+
+data class Coordinador(
+    val idC: Int,
+    val nombres: String,
+    val apellidos: String,
+    val fechaNac: String,
+    val titulo: String,
+    val email: String,
+    val facultad: String)
